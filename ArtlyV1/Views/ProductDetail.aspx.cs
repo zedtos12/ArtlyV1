141fd98aa6f42c7e20c2a0f08e4eae6919f72228
Felix Wongso <wongso.felix@gmail.com>
2024-05-20 14:36:19 +0700

    Product Detail Page stash

---
+++ ArtlyV1/Views/ProductDetail.aspx.cs
@@ -0,0 +1,21 @@
+﻿using ArtlyV1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArtlyV1.Views
+{
+    public partial class ProductDetail : System.Web.UI.Page
+    {
+        ProductDetailController detailController = new ProductDetailController();
+        public String productName;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
