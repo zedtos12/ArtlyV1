cc859433e8a4fee778a3079a4e514a291b671b5f
Clement Lewi LImuel <clementlimuel@gmail.com>
2024-05-22 00:36:58 +0700

    UpdateDB

---
+++ ArtlyV1/Handlers/ProfilePageHandler.cs
@@ -0,0 +1,22 @@
+﻿using ArtlyV1.Models;
+using ArtlyV1.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtlyV1.Handlers
+{
+    public class ProfilePageHandler
+    {
+        ArtlyDatabaseEntities db = DatabaseSingleton.getInstance();
+
+      
+
+        public List<TrUserAddress> GetTrUserAddresses(String userID)
+        {
+            List<TrUserAddress> userAddresses = (from x in db.TrUserAddresses where x.IdUser == userID select x).ToList();
+            return userAddresses;
+        }
+    }
+}
--- ArtlyV1/Models/ArtlyDBModel.Designer.cs
+++ ArtlyV1/Models/ArtlyDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Felix Wongso\Source\Repos\zedtos12\ArtlyV1\ArtlyV1\Models\ArtlyDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\cleme\source\repos\ClementBranch\ArtlyV1\Models\ArtlyDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
