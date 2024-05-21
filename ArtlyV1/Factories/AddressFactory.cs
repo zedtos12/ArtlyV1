d76c281c2ccd56816c806ca28a07dad75f7956e9
Clement Lewi LImuel <clementlimuel@gmail.com>
2024-05-22 00:45:02 +0700

    AddressFunctionalityBackend

---
+++ ArtlyV1/Factories/AddressFactory.cs
@@ -0,0 +1,24 @@
+﻿using ArtlyV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtlyV1.Factories
+{
+    public class AddressFactory
+    {
+        public static TrUserAddress create(String addressID, String userID, String address, String addressName)
+        {
+            TrUserAddress userAddress = new TrUserAddress()
+            {
+                IdAddress = addressID,
+                IdUser = userID,
+                Address = address,
+                AddressName = addressName
+            };
+
+            return userAddress;
+        }
+    }
+}
