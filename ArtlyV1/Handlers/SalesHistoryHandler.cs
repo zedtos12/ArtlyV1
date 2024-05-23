e5e0ad825c4e9ca38239deda1d8f88a0d57f0cf0
WataHata <95978721+WataHata@users.noreply.github.com>
2024-05-24 06:21:11 +0700

    Progress on HistoryPage

---
+++ ArtlyV1/Handlers/SalesHistoryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ArtlyV1.Models;
+using ArtlyV1.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtlyV1.Handlers
+{
+    public class SalesHistoryHandler
+    {
+        ArtlyDatabaseEntities db = DatabaseSingleton.getInstance();
+
+        public List<TransactionDetail> findSalesByUsername(String username)
+        {
+            string userId = "specificUserId"; // Replace with the specific UserId
+
+            List<TransactionDetail> lst = (from td in db.TransactionDetails
+                                           join p in db.Products on td.IdProduct equals p.IdProduct
+                                           where p.IdProductOwner == userId
+                                           select td).ToList();
+            return lst;
+        }
+    }
+}
