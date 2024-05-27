a1954719207218668368a41d2fe8c9cacdd7077b
WataHata <95978721+WataHata@users.noreply.github.com>
2024-05-28 01:07:08 +0700

    Progress on MessagePage

---
+++ ArtlyV1/Factories/MessageFactory.cs
@@ -0,0 +1,24 @@
+﻿using ArtlyV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace ArtlyV1.Factories
+{
+    public class MessageFactory
+    {
+        public static Message Create(string IdMessage, string IdSender, string IdReceiver, string MessageContent, DateTime MessageDate)
+        {
+            Message message = new Message();
+            message.IdMessage = IdMessage;
+            message.IdSender = IdSender;
+            message.IdReceiver = IdReceiver;
+            message.Content = MessageContent;
+            message.timestamp = MessageDate;
+            message.IsActive = true;
+            return message;
+        }
+    }
+}
