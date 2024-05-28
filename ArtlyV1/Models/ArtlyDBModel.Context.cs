1e46c4f570a60045a62cee1642efe9e8421b94ac
Bonifasius Wijaya Oey <103344796+zedtos12@users.noreply.github.com>
2024-05-28 14:36:46 +0700

    Push Fix Master + IsActive DB

--- ArtlyV1/Models/ArtlyDBModel.Context.cs
+++ ArtlyV1/Models/ArtlyDBModel.Context.cs
@@ -9,15 +9,19 @@
 
 namespace ArtlyV1.Models
 {
+    using ArtlyV1.Repository;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    
+    using System.Linq;
+    using System.Linq.Expressions;
+
     public partial class ArtlyDatabaseEntities : DbContext
     {
         public ArtlyDatabaseEntities()
             : base("name=ArtlyDatabaseEntities")
         {
+            DbConfiguration.SetConfiguration(new ArtlyDatabaseConfiguration());
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -38,5 +42,38 @@ namespace ArtlyV1.Models
         public virtual DbSet<TopUp> TopUps { get; set; }
         public virtual DbSet<TransactionDetail> TransactionDetails { get; set; }
         public virtual DbSet<MsProduct> MsProducts { get; set; }
+
+        public IQueryable<T> ActiveEntities<T>() where T : class
+        {
+            var query = Set<T>().AsQueryable();
+            var parameter = Expression.Parameter(typeof(T), "e");
+            var property = typeof(T).GetProperty("IsActive");
+
+            if (property != null)
+            {
+                // If IsActive is nullable, we need to handle the conversion
+                var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+                var constant = Expression.Constant(true, property.PropertyType);
+
+                Expression equality;
+                if (property.PropertyType == typeof(bool?))
+                {
+                    var hasValue = Expression.Property(propertyAccess, "HasValue");
+                    var value = Expression.Property(propertyAccess, "Value");
+                    var notNull = Expression.Equal(hasValue, Expression.Constant(true));
+                    var equalsTrue = Expression.Equal(value, Expression.Constant(true));
+                    equality = Expression.AndAlso(notNull, equalsTrue);
+                }
+                else
+                {
+                    equality = Expression.Equal(propertyAccess, constant);
+                }
+
+                var lambda = Expression.Lambda<Func<T, bool>>(equality, parameter);
+                query = query.Where(lambda);
+            }
+
+            return query;
+        }
     }
 }
