58d911f44c54a7ccc1e1fecb3ca8451614d10c5c
Felix Wongso <wongso.felix@gmail.com>
2024-06-02 15:01:21 +0700

    Add Factories

---
+++ ArtlyV1/Factories/TopUpFactory.cs
@@ -0,0 +1,24 @@
+﻿using ArtlyV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtlyV1.Factories
+{
+    public class TopUpFactory
+    {
+        public static TopUp create(String topUpID, String userID, decimal amount, DateTime date)
+        {
+            TopUp topUp = new TopUp()
+            {
+                IdTopUp = topUpID,
+                IdUser = userID,
+                Amount = amount,
+                TopUpDate = date
+            };
+
+            return topUp;
+        }
+    }
+}
---
+++ ArtlyV1/Factories/TransactionFactory.cs
@@ -0,0 +1,26 @@
+﻿using ArtlyV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtlyV1.Factories
+{
+    public class TransactionFactory
+    {
+        public static MsTransaction create(String transactionID, String userID, DateTime orderDate, String statusID, String paymentMethodID, String trackingID)
+        {
+            MsTransaction transaction = new MsTransaction()
+            {
+                IdTransaction = transactionID,
+                IdUser = userID,
+                OrderDate = orderDate,
+                IdStatus = statusID,
+                IdPaymentMethod = paymentMethodID,
+                TrackingId = trackingID
+            };
+
+            return transaction;
+        }
+    }
+}
